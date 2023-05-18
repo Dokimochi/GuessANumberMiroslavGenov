903b53963e6d62221bd216b47bcc7146af8a71d4
Dokimochi <93315855+Dokimochi@users.noreply.github.com>
2023-05-18 15:03:43 +0300

    Add files via upload

---
+++ GuessANumber.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Guess a number (1 - 100): ");
+    string input = Console.ReadLine();
+
+    bool isValid = int.TryParse(input, out int playerNumber);
+
+    if (isValid)
+    {
+        if (randomNumber == playerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (randomNumber < playerNumber)
+        {
+            Console.WriteLine("Too High!");
+            continue;
+        }
+        else if (randomNumber > playerNumber)
+        {
+            Console.WriteLine("Too Low!");
+            continue;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Wrong input!");
+        continue;
+    }
+}
